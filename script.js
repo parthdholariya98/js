4e6ff5f3e49546eba8bdc8f4d341c9c78dd2c20d
Parth Dholariya <parthdholariya98@gmail.com>
2025-06-22 11:26:42 +0530

    web

---
+++ script.js
@@ -0,0 +1,8 @@
+// let accountEmail = "abc#hc.com";
+// let accountName = "abc";
+// let accountPassword = "12548";
+// let accountCity = "Jaypur";
+
+// console.table([accountEmail, accountName, accountCity, accountPassword]);
+
+console.log();
