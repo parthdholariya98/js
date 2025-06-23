c336f9c81e2bca202a0c32fb29135aca7f0c80a3
Parth Dholariya <parthdholariya98@gmail.com>
2025-06-23 09:14:09 +0530

    logo

--- script.js
+++ script.js
@@ -5,5 +5,16 @@
 
 // console.table([accountEmail, accountName, accountCity, accountPassword]);
 
-console.log(+true);
-console.log("");
+// console.log(+true);
+// console.log(+"");
+
+
+// let gamecounter = 100;
+// gamecounter-- ;
+// console.log(gamecounter);
+
+console.log(null <= 1);
+
+let str = "how do ypu do";
+
+
