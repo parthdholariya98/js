b36db3fa18bd7748a338bc4309982d191ccef939
Parth Dholariya <parthdholariya98@gmail.com>
2025-06-25 12:50:16 +0530

    js_update

--- script.js
+++ script.js
@@ -13,8 +13,91 @@
 // gamecounter-- ;
 // console.log(gamecounter);
 
-console.log(null <= 1);
 
-let str = "how do ypu do";
+// const myName = "myName";
+
+// console.log(myName);
+
+// const ontherName = myName.substring(0, 6);
+// console.log(ontherName);
+
+// const moreName = myName.slice(-6, 2);
+// console.log(moreName);
+
+// const newString = "  newString  "
+// console.log(newString);
+// console.log(newString.trim());
 
 
+// let subject = "javascript";
+// console.log(subject);
+
+// const ans = subject.split('').reverse('').join('');
+
+// console.log(ans);
+
+// const score = 100;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// const  min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max -min + 1)) + min);
+
+
+
+// let myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate);
+
+// let mycreatedate = new Date("1-14-2024");
+// console.log(mycreatedate.toLocaleDateString());
+
+
+// console.log(mycreatedate.getTime());
+
+// let newDate = new Date();
+// console.log(newDate.getMonth() + 1);
+
+// const string = " Hello Word ! ";
+
+// let ans = string.trimStart();
+// console.log(ans);
+
+
+// const same = (a,b) => {
+//     console.log(same);
+// }
+
+// console.log(same);
+
+// const my_car  = ["car_1", "car_2", "car_3"];
+// const my_cars = ["car_4", "car_5", "car_6"];
+
+// const new_car = my_car.concat(my_cars);
+// console.log(new_car);
+
+// const number = [1, 2, 3, [4, 5, 6,], 7, [8, 9,], 10];
+// const real_number = number.flat(Infinity);
+// console.log(real_number);
+
+
+
+// console.log(Array.isArray("parth"));
+// console.log(Array.from("parth"));
+
+
+let score = 100
+let score_1 = 200
+let score_2 = 300
+let score_3 = 400
+let score_4 = 500
+
+console.log(Array.of(score, score_1, score_2, score_3, score_4));
